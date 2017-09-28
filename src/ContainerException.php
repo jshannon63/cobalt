ccb89235955203906e97ce07a60ba3ef440da21a
jshannon63 <jim@hltky.com>
2017-09-27 22:53:43 -0400

    StyleCI repairs

--- src/ContainerException.php
+++ src/ContainerException.php
@@ -10,7 +10,4 @@ namespace Jshannon63\Container;
 use Exception;
 use Psr\Container\ContainerExceptionInterface;
 
-class ContainerException extends Exception implements ContainerExceptionInterface
-{
-    //
-}
+class ContainerException extends Exception implements ContainerExceptionInterface {}
